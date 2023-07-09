e6c9830ee261a02b050bfd1a00882549f142ecb9
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2023-07-09 18:06:59 +0530

    TableOfNumber

---
+++ java/ch2/TableOfNumber.java
@@ -0,0 +1,14 @@
+package ch2;
+
+import java.util.Scanner;
+
+public class TableOfNumber {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        for (int i=1;i<=10;i++) {
+            System.out.print(n*i + " ");
+        }
+    }
+}
