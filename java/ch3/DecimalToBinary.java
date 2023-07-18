c379891b3900c2f3ed3b0d46329de472c8a99973
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2023-07-18 23:03:57 +0530

    dec2bin

---
+++ java/ch3/DecimalToBinary.java
@@ -0,0 +1,13 @@
+package ch3;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        String binary = "";
+        int n = 10;
+        while (n>0) {
+            binary = (n%2) + binary;
+            n = n/2;
+        }
+        System.out.println(binary);
+    }
+}
