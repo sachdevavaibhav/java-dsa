df192b124b29b74ab8169cd1212141e5fb5d87c5
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2023-08-03 22:14:39 +0530

    toBinary func

--- java/ch3/DecimalToBinary.java
+++ java/ch3/DecimalToBinary.java
@@ -3,12 +3,17 @@ package ch3;
 
 public class DecimalToBinary {
     public static void main(String[] args) {
-        String binary = "";
         int n = 10;
+        String binary = toBinary(n);
+        System.out.println(binary);
+    }
+
+    public static String toBinary(int n) {
+        String binary = "";
         while (n>0) {
             binary = (n%2) + binary;
             n = n/2;
         }
-        System.out.println(binary);
+        return binary;
     }
 }
