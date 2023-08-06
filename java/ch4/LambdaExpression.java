2f48835827882f4245c6d8eeffe6d50edbf789c9
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2023-08-06 16:53:44 +0530

    lambda expressions intro

---
+++ java/ch4/LambdaExpression.java
@@ -0,0 +1,31 @@
+package ch4;
+
+import java.util.function.Predicate;
+
+public class LambdaExpression {
+    public static void main(String[] args) {
+        // without lambda expressions
+        // class myPredicate implements Predicate<Integer> {
+        //     public boolean test(Integer x) {
+        //         return x%2==0;
+        //     }
+        // }
+        // int[] arr = {1,2,3,4};
+        // printCond(arr, new myPredicate());
+        // with lambda expressions
+        int[] arr = {1,2,3,4};
+        printCond(arr, x -> x%2==0); //the function passed in the second argument will become the implementation of abstract method of Predicate functional interface.
+        /*
+         * Lambda expressions basically express instances of functional interfaces (An interface with a single abstract method is called a functional interface). 
+         */
+    }   
+    
+    public static void printCond(int[] arr, Predicate<Integer> p) {
+        for (int x: arr) {
+            Integer y = Integer.valueOf(x);
+            if (p.test(y)) {
+                System.out.print(y+" ");
+            }
+        }
+    }
+}
