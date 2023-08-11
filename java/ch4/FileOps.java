1337eceac1dcdac64b36e8ed207cb2b724e5b19b
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2023-08-12 00:14:09 +0530

    file operations

--- java/ch4/FileOps.java
+++ java/ch4/FileOps.java
@@ -1,13 +1,18 @@
 package ch4;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
-public class FileCreate {
+public class FileOps {
     public static void main(String[] args) {
         String path = "./java/ch4/create.txt";
+        String writePath = "./java/ch4/output.txt";
         create(path);
         fileMethods(path);
+        write(writePath);
+        read(writePath);
     }
 
     public static void create(String filePathName) {
@@ -35,6 +40,31 @@ public class FileCreate {
         }
         else {
             System.out.println("File does not exists.");
+        }
+    }
+
+    public static void write(String filePath) {
+        try {
+            FileWriter wr = new FileWriter(filePath);
+            wr.write("Hello world to file handling in java.");
+            wr.close();
+            System.out.println("Written Successfully");
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+    }
+
+    public static void read(String filePath) {
+        try {
+            File obj = new File(filePath);
+            Scanner sc = new Scanner(obj);
+            while (sc.hasNextLine()) {
+                String data = sc.nextLine();
+                System.out.println(data);
+            }
+            sc.close();
+        } catch (Exception e) {
+            System.out.println("error");
         }
     }
 }
