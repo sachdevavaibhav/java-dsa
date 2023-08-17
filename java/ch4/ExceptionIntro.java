a8d2fb5743ef13a62f8f2e12822007e1d6e2a636
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2023-08-18 01:43:08 +0530

    unchecked exceptions

--- java/ch4/ExceptionIntro.java
+++ java/ch4/ExceptionIntro.java
@@ -6,10 +6,12 @@ import java.util.Scanner;
 
 public class ExceptionIntro {
     public static void main(String[] args) {
-        fileHandle();
+        // fileHandle();
+        sum(args);
     }
 
     public static void fileHandle() {
+        // checked exceptions - which are checked at compiled time (part of Exception class)
         try {
             Scanner sc = new Scanner(new File("test.in"));
             sc.close();
@@ -19,6 +21,20 @@ public class ExceptionIntro {
         }
         finally {
             System.out.println("In the final block");
+        }
+    }
+
+    public static void sum(String args[]) {
+        // unchecked exceptions (part of Errors (which depend of external factors) class and RuntimeExceptions(subclass of Exceptions) class)
+        int a, b, sum;
+        try {
+            a = Integer.parseInt(args[0]);
+            b = Integer.parseInt(args[1]);
+            sum=a+b;
+            System.out.println("Result: " + sum);
+        }
+        catch (ArrayIndexOutOfBoundsException err) {
+            System.out.println("Insufficient numbers provided.");
         }
     }
 }
