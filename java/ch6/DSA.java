92fdad861e0aa1f60b5d7e4244cbb86cde59e26f
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2023-09-12 23:48:56 +0530

    dsa intro

---
+++ java/ch6/DSA.java
@@ -0,0 +1,12 @@
+package ch6;
+public class DSA {
+    // What is data structure?
+    // A data structure is a way to store and organize data in order to facilitate access and modifications.
+    // Data structures are designed to organize data to suit a specific purpose.
+    // Data structures are used in almost every program or software system.
+    // Data structures are used to store data in a computer in an organized form.
+    // Data structures are used to implement efficient algorithms.
+    // What is algorithm?
+    // An algorithm is a set of instructions to solve a problem.
+    // An algorithm is a finite set of instructions that, if followed, accomplish a particular task.
+}
