8292298c901377bfe97f7d8507e0106a8b20bc84
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2023-10-26 15:18:55 +0530

    tower of hanoi problem

---
+++ basicDsa/recursion/TowerOfHanoi.java
@@ -0,0 +1,16 @@
+package basicDsa.recursion;
+
+public class TowerOfHanoi {
+    public static void towerOfHanoi(int n, char src, char aux, char dest) {
+        if (n==1) {
+            System.out.println("Moving " + n + " from "+src+" -> "+dest);
+            return;
+        }
+        towerOfHanoi(n-1, src, dest, aux);
+        System.out.println("Moving " + n + " from "+src+" -> "+dest);
+        towerOfHanoi(n-1, aux, src, dest);
+    }
+    public static void main(String[] args) {
+        towerOfHanoi(4, 'A', 'B', 'C');
+    }
+}
